73d01c3d83c2ac83171506eaa0be5ff2e6c2484c
Johannes Thoma <johannes@johannesthoma.com>
2025-03-29 18:57:58 +0000

    Started working on Windows architecture.

---
+++ arch/windows/include/uapi/asm/byteorder.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0 WITH Linux-syscall-note */
+#ifndef _ASM_WINDOWS_BYTEORDER_H
+#define _ASM_WINDOWS_BYTEORDER_H
+
+/* TODO: if ARM ... */
+#include <linux/byteorder/little_endian.h>
+
+#endif /* _ASM_X86_BYTEORDER_H */
+

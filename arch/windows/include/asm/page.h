8d7c2a815aa7ff08397cfa00bcc564b3f1aea87a
Johannes Thoma <johannes@johannesthoma.com>
2025-09-25 11:02:59 +0000

    Made the page related things compile.
    With dummy definitions.

--- arch/windows/include/asm/page.h
+++ arch/windows/include/asm/page.h
@@ -5,6 +5,35 @@
 #define THREAD_SIZE PAGE_SIZE
 #define TASK_SIZE 0x80000000
 
-typedef int pgtable_t;
-typedef int pgprot_t;
-typedef int pgd_t;
+/* taken from arm/include/asm/page_nommu.h */
+#define clear_page(page)	memset((page), 0, PAGE_SIZE)
+#define copy_page(to,from)	memcpy((to), (from), PAGE_SIZE)
+
+#define clear_user_page(page, vaddr, pg)	clear_page(page)
+#define copy_user_page(to, from, vaddr, pg)	copy_page(to, from)
+
+typedef struct page *pgtable_t;
+
+/*
+ * These are used to make use of C type-checking..
+ */
+typedef unsigned long pte_t;
+typedef unsigned long pmd_t;
+typedef unsigned long pgd_t[2];
+typedef unsigned long pgprot_t;
+
+#define pte_val(x)      (x)
+#define pmd_val(x)      (x)
+#define pgd_val(x)	((x)[0])
+#define pgprot_val(x)   (x)
+
+#define __pte(x)        (x)
+#define __pmd(x)        (x)
+#define __pgprot(x)     (x)
+
+#include <asm-generic/pgtable-nopud.h>
+
+#define virt_to_page(addr) (struct page*)(0)
+#define page_to_pfn(addr) (0)
+#define pfn_to_page(addr) (struct page*)(0)
+#define __va(phys) (0)

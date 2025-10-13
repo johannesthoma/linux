50f07e935ef107b873dfffc3af0811d6f33e4a95
Johannes Thoma <johannes@johannesthoma.com>
2025-10-13 17:09:48 +0000

    Include ReactOS headers (again).

--- arch/windows/windows/entry.c
+++ arch/windows/windows/entry.c
@@ -1,13 +1,11 @@
-/*
 #include <ntdef.h>
 #include <ntddk.h>
 #include <ntdddisk.h>
-*/
 
-typedef int NTSTATUS;
-#define STATUS_SUCCESS 0
+// typedef int NTSTATUS;
+// #define STATUS_SUCCESS 0
 
-extern int DbgPrint(const char *fmt, ...);
+// extern int DbgPrint(const char *fmt, ...);
 
 // NTSTATUS __attribute__((stdcall)) DriverEntry(IN PDRIVER_OBJECT DriverObject, IN PUNICODE_STRING registry_path)
 NTSTATUS __attribute__((stdcall)) DriverEntry(void *DriverObject, void *registry_path)

78a534fc9ef9ac61ba66d149ea9099da4ae4eddd
Johannes Thoma <johannes@johannesthoma.com>
2025-10-13 17:06:30 +0000

    Imported ReactOS headers.
    Still need to link manually (see comment in arch/windows/Makefile)
    but DbgPrint can be linked and called correctly.

---
+++ arch/windows/windows/entry.c
@@ -0,0 +1,18 @@
+/*
+#include <ntdef.h>
+#include <ntddk.h>
+#include <ntdddisk.h>
+*/
+
+typedef int NTSTATUS;
+#define STATUS_SUCCESS 0
+
+extern int DbgPrint(const char *fmt, ...);
+
+// NTSTATUS __attribute__((stdcall)) DriverEntry(IN PDRIVER_OBJECT DriverObject, IN PUNICODE_STRING registry_path)
+NTSTATUS __attribute__((stdcall)) DriverEntry(void *DriverObject, void *registry_path)
+{
+	DbgPrint("Hallo Linux!!\n");
+	return STATUS_SUCCESS;
+}
+

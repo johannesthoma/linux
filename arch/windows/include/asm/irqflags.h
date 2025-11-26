f56499dbad5ee99e144e2436db0063be88b50b9f
Johannes Thoma <johannes@johannesthoma.com>
2025-11-26 16:46:13 +0000

    Working on irqflags.h
    Arghh..cannot include a windows header (crt/mingw32/intrin_x86.h)
    outside the windows subfolder .. will revert to the x86 arch
    irqflags.h.

--- arch/windows/include/asm/irqflags.h
+++ arch/windows/include/asm/irqflags.h
@@ -1,2 +1,108 @@
-#include <asm-generic/irqflags.h>
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _X86_IRQFLAGS_H_
+#define _X86_IRQFLAGS_H_
 
+#ifndef __ASSEMBLY__
+
+#include <crt/mingw32/intrin_x86.h>
+
+/*
+ * Interrupt control:
+ */
+
+/* Declaration required for gcc < 4.9 to prevent -Werror=missing-prototypes */
+extern inline unsigned long native_save_fl(void);
+extern __always_inline unsigned long native_save_fl(void)
+{
+	return __readeflags();
+}
+
+static __always_inline void native_irq_disable(void)
+{
+	_disable();
+}
+
+static __always_inline void native_irq_enable(void)
+{
+	_enable();
+}
+
+static __always_inline void native_safe_halt(void)
+{
+	__halt();
+//	asm volatile("sti; hlt": : :"memory");
+}
+
+static __always_inline void native_halt(void)
+{
+	__halt();
+//	asm volatile("hlt": : :"memory");
+}
+
+#include <linux/types.h>
+
+static __always_inline unsigned long arch_local_save_flags(void)
+{
+	return native_save_fl();
+}
+
+static __always_inline void arch_local_irq_disable(void)
+{
+	native_irq_disable();
+}
+
+static __always_inline void arch_local_irq_enable(void)
+{
+	native_irq_enable();
+}
+
+/*
+ * Used in the idle loop; sti takes one instruction cycle
+ * to complete:
+ */
+static __always_inline void arch_safe_halt(void)
+{
+	native_safe_halt();
+}
+
+/*
+ * Used when interrupts are already enabled or to
+ * shutdown the processor:
+ */
+static __always_inline void halt(void)
+{
+	native_halt();
+}
+
+/*
+ * For spinlocks, etc:
+ */
+static __always_inline unsigned long arch_local_irq_save(void)
+{
+	unsigned long flags = arch_local_save_flags();
+	arch_local_irq_disable();
+	return flags;
+}
+
+#define THE_X86_EFLAGS_IF (1UL << 9)
+
+static __always_inline int arch_irqs_disabled_flags(unsigned long flags)
+{
+	return !(flags & THE_X86_EFLAGS_IF);
+}
+
+static __always_inline int arch_irqs_disabled(void)
+{
+	unsigned long flags = arch_local_save_flags();
+
+	return arch_irqs_disabled_flags(flags);
+}
+
+static __always_inline void arch_local_irq_restore(unsigned long flags)
+{
+	if (!arch_irqs_disabled_flags(flags))
+		arch_local_irq_enable();
+}
+#endif /* !__ASSEMBLY__ */
+
+#endif

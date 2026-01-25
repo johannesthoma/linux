ac9fea8c95aba9744b05e3608452e8fd5a5e6672
Johannes Khoshnazar-Thoma <johannes@johannesthoma.com>
2026-01-25 16:53:18 +0100

    Cleanup weak patch
    The idea would be to fix mingw-w64 (or the linker) to support
    weak definition after non-weak declaration.

--- kernel/bpf/core.c
+++ kernel/bpf/core.c
@@ -37,17 +37,9 @@
 #include <linux/nospec.h>
 #include <linux/bpf_mem_alloc.h>
 #include <linux/memcontrol.h>
-// #include <linux/compiler_attributes.h>
 
 #include <asm/barrier.h>
 #include <asm/unaligned.h>
-
-/*
-#ifdef CONFIG_WINDOWS
-#undef __weak 
-#define __weak
-#endif
-*/
 
 /* Registers */
 #define BPF_R0	regs[BPF_REG_0]
@@ -2839,7 +2831,7 @@ bpf_event_output(struct bpf_map *map, u64 flags, void *meta, u64 meta_size,
 {
 	return -ENOTSUPP;
 }
- EXPORT_SYMBOL_GPL(bpf_event_output);
+EXPORT_SYMBOL_GPL(bpf_event_output);
 
 /* Always built-in helper functions. */
 const struct bpf_func_proto bpf_tail_call_proto = {
@@ -2863,7 +2855,7 @@ struct bpf_prog * __weak bpf_int_jit_compile(struct bpf_prog *prog)
 /* Stub for JITs that support eBPF. All cBPF code gets transformed into
  * eBPF by the kernel and is later compiled by bpf_int_jit_compile().
  */
-void __weak bpf_jit_compile(struct bpf_prog *prog)
+void /* __weak */ bpf_jit_compile(struct bpf_prog *prog)
 {
 }
 
@@ -2906,13 +2898,11 @@ bool __weak bpf_jit_supports_far_kfunc_call(void)
 /* To execute LD_ABS/LD_IND instructions __bpf_prog_run() may call
  * skb_copy_bits(), so provide a weak definition of it for NET-less config.
  */
-/*
 int __weak skb_copy_bits(const struct sk_buff *skb, int offset, void *to,
 			 int len)
 {
 	return -EFAULT;
 }
-*/
 
 int __weak bpf_arch_text_poke(void *ip, enum bpf_text_poke_type t,
 			      void *addr1, void *addr2)

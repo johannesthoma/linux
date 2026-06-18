b4bc2fc866e509d93cae06d0b78f92356ef46c4a
Johannes Khoshnazar-Thoma <johannes@johannesthoma.com>
2026-06-18 11:59:41 +0200

    Debug print whenever write(2) is called.

--- fs/read_write.c
+++ fs/read_write.c
@@ -628,6 +628,8 @@ ssize_t ksys_write(unsigned int fd, const char __user *buf, size_t count)
 	struct fd f = fdget_pos(fd);
 	ssize_t ret = -EBADF;
 
+DbgPrint("writing %d bytes from %p ...\n", count, buf);
+
 	if (f.file) {
 		loff_t pos, *ppos = file_ppos(f.file);
 		if (ppos) {
